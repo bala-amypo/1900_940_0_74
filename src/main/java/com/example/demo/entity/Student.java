b4a074e2631f5f5550f86ceacd05fdc7225a79e7
bala-amypo <bala@amypo.in>
2025-12-17 05:33:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,15 +1,19 @@
 package com.example.demo.entity;
+import jakarta.persistence.*;
 
+@entity
+@table(name="students")
 import java.time.LocalDate;
+
 public class Student {
-    private Long id;
+    private long id;
     private String name;
     private String email;
     
-    public int getId() {
+    public long getId() {
         return id;
     }
-    public void setId(Long id) {
+    public void setId(long id) {
         this.id = id;
     }
     public String getName() {
@@ -25,14 +29,20 @@ public class Student {
         this.email = email;
     }
     
-    public Student(Long id, String name, String email, LocalDate dob, float cgpa) {
+    public Student(long id, String name, String email, LocalDate dob, float cgpa) {
         this.id = id;
         this.name = name;
         this.email = email;
         
     }
-    public Student(){
-
+    public Student() {
+        public void setId(int id) { 
+           this.id = id;
+    }
+    public int getId() { //Getting the data Id
+           return id;
+    }
+    
     }
 
 }
